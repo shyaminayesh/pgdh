fc340c1a45b5208def96baaaae2554533a24ceba
Shyamin Ayesh <me@shyamin.com>
2019-01-22 21:35:18 +0530

    Basic test configuration

---
+++ tests/tests.php
@@ -0,0 +1,14 @@
+<?php
+
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+// INCLUDE CLASS FILES
+include_once '../pgdh.class.php';
+
+// INSTANCE
+$pgdh = new pgdh('resources/1.jps');
+
+echo "\nDONE\n\n";
+
+?>
