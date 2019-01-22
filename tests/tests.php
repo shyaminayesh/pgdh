6d4c66ce9c581ee1cf24a514b685559778c0f08d
Shyamin Ayesh <me@shyamin.com>
2019-01-22 22:07:53 +0530

    PATH -> TravisCI

--- tests/tests.php
+++ tests/tests.php
@@ -4,7 +4,7 @@ error_reporting(-1);
 ini_set('display_errors', 1);
 
 // INCLUDE CLASS FILES
-include_once __DIR__.'../pgdh.class.php';
+include_once __DIR__.'/../pgdh.class.php';
 
 // INSTANCE
 $pgdh = new pgdh( 'resources/1.jpg');
